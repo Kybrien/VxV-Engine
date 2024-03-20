9e15b7f5ca83e39e708ccb0b27f366173f28e394
unknown <ycaillot@gamingcampus.local>
2024-03-20 11:32:45 +0100

    GameObject v0

---
+++ VxV/GameObject.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <string>
+
+
+#include "Component.h"
+
+
+class GameObject 
+{
+public:
+	std::string name;
+
+	virtual void Init();
+	virtual void Start();
+	virtual void Update();
+
+	int GetId() { return id; }
+
+	GameObject* GetChildByName(std::string name);
+
+	template<typename T>
+	T* GetComponent();
+
+
+private:
+	int id;
+
+
+	std::list<Component*> components;
+	std::list<GameObject*> childObjects;
+};
