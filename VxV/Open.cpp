9ead554ecbe302eeb45568eedb6e66fe984f1170
46Yuu <htran@gaming.tech>
2024-03-20 12:05:53 +0100

    omg ca marche ?

--- VxV/Open.cpp
+++ VxV/Open.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <gl/glew.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
+#include <GLM/glm.hpp>
 
 using namespace glm;
 
