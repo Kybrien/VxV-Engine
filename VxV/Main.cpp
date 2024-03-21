9c9018224905935ad2bead23da217600b57375f5
46Yuu <htran@gaming.tech>
2024-03-21 09:41:06 +0100

    push tuto2

--- VxV/Main.cpp
+++ VxV/Main.cpp
@@ -6,6 +6,8 @@
 #include <GLFW/glfw3.h>
 #include <GLM/glm.hpp>
 
+#include <../../../loadShader.hpp>
+
 using namespace glm;
 
 int main() {
@@ -21,13 +23,13 @@ int main() {
 
 	// Open a window and create its OpenGL context
 	GLFWwindow* window;
-	window = glfwCreateWindow(1024, 768, "VxV", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "VxV", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window\n");
 		glfwTerminate();
 		return -1;
 	}
-
+	
 	glfwMakeContextCurrent(window);// Initialize GLEW
 
 	glewExperimental = true;
@@ -39,8 +41,46 @@ int main() {
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	static const GLfloat g_vertex_buffer_data[] = {
+	   -1.0f, -1.0f, 1.0f,
+	   1.0f, -1.0f, 1.0f,
+	   0.0f,  1.0f, 1.0f,
+	};
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("SimpleVertexShader.MIKU", "SimpleFragmentShader.VALORANT");
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 	do {
-		glClear(GL_COLOR_BUFFER_BIT);
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+		// Draw triangle...
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+
+		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
