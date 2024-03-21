827affb6e5a849842fe414f034fd34733edd8f7c
unknown <ycaillot@gamingcampus.local>
2024-03-21 09:31:00 +0100

    merge API & fenetre

--- VxV/Main.cpp
+++ VxV/Main.cpp
@@ -1,15 +1,49 @@
-#include "GameObject.h"
-#include "Debug.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
 
-int main()
-{
-	GameObject test;
+using namespace glm;
 
-	test.name = "test";
+int main() {
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
 
-	Debug::Log(test.name);
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
 
+	// Open a window and create its OpenGL context
+	GLFWwindow* window;
+	window = glfwCreateWindow(1024, 768, "VxV", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);// Initialize GLEW
+
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
 	return 0;
-}
+}	
