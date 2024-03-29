16239fcd30b8663d148adcecfcc2e2361db04579
Mguellaff <115532700+Mguellaff@users.noreply.github.com>
2024-03-29 13:37:43 +0100

    Update ScriptManager.cpp

--- VxV/ScriptManager.cpp
+++ VxV/ScriptManager.cpp
@@ -4,3 +4,11 @@ ScriptManager::ScriptManager(Manager* manager) {
 
 	manager->AddManager<ScriptManager>(this);
 }
+
+void ScriptManager::Save()
+{
+}
+
+void ScriptManager::Load(std::wstring wFileDirection)
+{
+}
