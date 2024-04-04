f81dc56bc5718c02bdc0c5d1de922294546b8d0c
Hroycode <115458097+Hroycode@users.noreply.github.com>
2024-04-04 10:47:12 +0200

    ui v2

--- VxV/EngineGUI.cpp
+++ VxV/EngineGUI.cpp
@@ -8,7 +8,17 @@ void EngineGUI::UpdateGui()
 
  // Fenêtre pour l'accès aux fichiers
     ShowFileExplorer(); 
+    // Début de la fenêtre transparente
+    ImGui::SetNextWindowBgAlpha(0.5f); // Rendre la fenêtre semi-transparente
+    ImGui::Begin("Statistiques", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove);
 
+    // Afficher les FPS
+    ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+
+    // Afficher la RAM consommée en pourcentage
+    ImGui::Text("RAM: %.1f%%", (float)(ImGui::GetIO().MetricsAllocs * 100) / ImGui::GetIO().MetricsAllocsMax);
+
+    ImGui::End();
 
 
 }
