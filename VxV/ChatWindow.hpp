a5b16184f6c97e7c170fba83b7df30b3a0e5a0e5
Adri <adrienpatte75@gmail.com>
2024-04-23 12:06:02 +0200

    ChatMaybe

--- VxV/ChatWindow.hpp
+++ VxV/ChatWindow.hpp
@@ -1,27 +1,107 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma once
+
 #include <deque>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string>
+#include <thread>
+#include <mutex>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <imgui.h>
 
-struct ChatWindow
-{
+#pragma comment(lib, "Ws2_32.lib")
+
+struct ChatWindow {
     std::deque<std::string> messages;
     char inputBuf[256];
+    SOCKET clientSocket;
+    std::thread receiveThread;
+    std::mutex mutex;
 
-    ChatWindow()
-    {
+    ChatWindow() {
         memset(inputBuf, 0, sizeof(inputBuf));
+        initNetwork();
     }
 
-    void Draw()
-    {
+    ~ChatWindow() {
+        closeNetwork();
+    }
+
+    void initNetwork() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            addMessage("Winsock initialization failed.");
+            return;
+        }
+
+        clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (clientSocket == INVALID_SOCKET) {
+            WSACleanup();
+            addMessage("Socket creation failed.");
+            return;
+        }
+
+        sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(12345);
+        InetPton(AF_INET, TEXT("10.3.102.50"), &serverAddr.sin_addr);
+
+        if (connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+            closesocket(clientSocket);
+            WSACleanup();
+            addMessage("Connection failed.");
+            return;
+        }
+
+        receiveThread = std::thread(&ChatWindow::receiveMessages, this);
+    }
+
+    void closeNetwork() {
+        closesocket(clientSocket);
+        WSACleanup();
+        if (receiveThread.joinable()) {
+            receiveThread.join();
+        }
+    }
+
+    void receiveMessages() {
+        char message[1024];
+        while (true) {
+            int iResult = recv(clientSocket, message, sizeof(message), 0);
+            if (iResult > 0) {
+                message[iResult] = '\0';
+                std::lock_guard<std::mutex> lock(mutex);
+                addMessage("Server: " + std::string(message));
+            }
+            else {
+                if (iResult == 0) {
+                    addMessage("Connection closed by server.");
+                }
+                else {
+                    addMessage("Error receiving message.");
+                }
+                break;
+            }
+        }
+    }
+
+    void addMessage(const std::string& message) {
+        std::lock_guard<std::mutex> lock(mutex);
+        messages.push_back(message);
+    }
+
+    void sendMessage(const std::string& message) {
+        int iResult = send(clientSocket, message.c_str(), message.length(), 0);
+        if (iResult == SOCKET_ERROR) {
+            addMessage("Error sending message.");
+        }
+    }
+
+    void Draw() {
         ImGui::Begin("Chat Window");
 
         // Display messages
-        for (auto& message : messages)
-        {
+        for (const auto& message : messages) {
             ImGui::TextUnformatted(message.c_str());
         }
 
@@ -31,37 +111,19 @@ struct ChatWindow
 
         // Text input
         bool reclaim_focus = false;
-        if (ImGui::InputText("Input", inputBuf, sizeof(inputBuf), ImGuiInputTextFlags_EnterReturnsTrue))
-        {
+        if (ImGui::InputText("Input", inputBuf, sizeof(inputBuf), ImGuiInputTextFlags_EnterReturnsTrue)) {
             std::string message = inputBuf;
-            if (!message.empty())
-            {
-                // Send the message over the network
+            if (!message.empty()) {
                 sendMessage(message);
                 reclaim_focus = true;
             }
-
-            // Clear the input text field
-            memset(inputBuf, 0, sizeof(inputBuf));
+            memset(inputBuf, 0, sizeof(inputBuf)); // Clear the input text field
         }
 
-        // Auto focus on the text input field
-        if (reclaim_focus)
+        if (reclaim_focus) {
             ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
+        }
 
         ImGui::End();
-    }
-
-    // Add a method to add a message to the chat history
-    void addMessage(const std::string& message)
-    {
-        messages.push_back(message);
-    }
-
-    // Method to send a message over the network
-    void sendMessage(const std::string& message)
-    {
-        messages.push_back(message);
-        // Code to send the message over the network...
     }
 };
