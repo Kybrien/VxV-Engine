13768dd6a9773ba717d733fe0a1947007acdf13d
Draknut <60214040+Draknut@users.noreply.github.com>
2024-04-25 19:18:01 +0200

    Hiérarchie

--- VxV/EngineSetup.hpp
+++
@@ -1,6 +0,0 @@
-//#pragma once
-//#include "Engine.hpp"
-
-//void startup(EngineGUI* _gui, APIopenGL* _apiGraphic);
-//void apiDrawLoopSetup(APIopenGL* _apiGraphic);
-//void checkCloseWindow(APIopenGL* _apiGraphic, Engine* _engine);
